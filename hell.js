5206959e9981f065c19d8c25e94e9305e3c2953c
root <root@DESKTOP-TA5P16G.localdomain>
2019-03-20 01:47:11 -0400

    dv

--- hell.js
+++ hell.js
@@ -10,8 +10,14 @@ var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://130.245.170.77:27017';
 
 
+MongoClient.connect(url,  { useNewUrlParser: true }, insert)
+function insert(err, db){
+	var dbo = db.db("mm");
+	var myobj = { name: "Anniec", address: "Anna 37" };
+  	dbo.collection("movie").insertOne(myobj)
+}
 
-
+console.log('successSSS')
 
 app.use(session({secret:'iloveit'}))
 
